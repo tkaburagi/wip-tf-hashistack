41e36c8e59ff4c96aa841a36b49e1bcb220a0224
Takayuki Kaburagi <t.kaburagi@me.com>
2019-05-24 12:50:02 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = " 0.12.0"
+}
+
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
+}
+
+module "infra" {
+  source = "modules/infra"
+}
